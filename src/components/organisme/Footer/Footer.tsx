21e093e5664e9c5a6885cfa09dd599ca1d58e12d
Aryan Salemabadi <aryanavatar1380@gmail.com>
2025-07-29 01:05:49 +0330

    final feature/layout

---
+++ src/components/organisme/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="w-full bg-black text-gray-400 text-center py-4 mt-auto">
+      <p className="text-sm">
+        Designed & developed by{" "}
+        <span className="text-[#A3E635] font-semibold">Aryan Salemabadi</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/organisme/Header/data.ts
@@ -0,0 +1,24 @@
+import { ItemsTypes } from "./types";
+
+export const items: ItemsTypes[] = [
+  {
+    id: 1,
+    title: "Home",
+    href: "#home",
+  },
+  {
+    id: 1,
+    title: "Features",
+    href: "#features",
+  },
+  {
+    id: 1,
+    title: "Pricing",
+    href: "#pricing",
+  },
+  {
+    id: 1,
+    title: "Contact",
+    href: "#pontact",
+  },
+];
