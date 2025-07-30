f732d45d45a370ea79fd565aab160a383885116c
Aryan Salemabadi <aryanavatar1380@gmail.com>
2025-07-30 21:46:19 +0330

    add contactus section

--- src/components/template/LandingPage/LandingPage.tsx
+++ src/components/template/LandingPage/LandingPage.tsx
@@ -2,7 +2,6 @@
 import dynamic from "next/dynamic";
 import HeroSection from "@/components/organisme/HeroSection/HeroSection";
 import HowItWorksSection from "@/components/organisme/HowItWorksSection/HowItWorksSection";
-import ContactUsSection from "@/components/organisme/ContactUsSection/ContactUsSection";
 
 
 const QuickStart = dynamic(
@@ -14,6 +13,11 @@ const WhyTurboTrackSection = dynamic(
   () => import("@/components/organisme/WhyTurboTrackSection/WhyTurboTrackSection"),
   { ssr: false, loading: () => <p>Loading...</p> }
 );
+
+const ContactUsSection = dynamic(
+  () => import("@/components/organisme/ContactUsSection/ContactUsSection"),
+  { ssr: false, loading: () => <p>Loading...</p> }
+)
 
 const LandingPage = () => {
   return (
